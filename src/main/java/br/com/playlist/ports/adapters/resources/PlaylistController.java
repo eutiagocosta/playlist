d89e062fd861fbf19bd35231428f73b58230b041
tiago.costa@beblue.com.br <tiago.costa@beblue.com.br>
2018-12-04 23:32:47 -0200

    Fixed annotation for mapping request

--- src/main/java/br/com/playlist/ports/adapters/resources/PlaylistController.java
+++ src/main/java/br/com/playlist/ports/adapters/resources/PlaylistController.java
@@ -2,6 +2,7 @@ package br.com.playlist.ports.adapters.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ public class PlaylistController {
 	@Autowired
 	private PlaylistApplicationService service;
 	
-	@RequestMapping("/city")
+	@GetMapping("/city")
 	public ResponseEntity<PlaylistData> getPlaylistByCity(@RequestParam(value = "name") String name) {
 		return ResponseEntity.ok(service.getByCity(name));
 	}
 	
-	@RequestMapping("/coordinates")
+	@GetMapping("/coordinates")
 	public ResponseEntity<PlaylistData> getPlaylistByCoordenates(@RequestParam(value = "lat") String lat,
 			  @RequestParam(value = "lon") String lon) {
 		return ResponseEntity.ok(service.getByCoordinates(lat, lon));
